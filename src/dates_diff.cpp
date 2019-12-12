af63af194d400a9c76644a2b6c08cca87cdfda21
Nikolai Kostromitin <emailbin@yandex.ru>
2019-12-12 12:22:32 +0500

    Minor change in comments
    Change and remove some comments

--- src/dates_diff.cpp
+++ src/dates_diff.cpp
@@ -1,12 +1,10 @@
-/* Рассчет разницы в днях между двумя датами.
+/* Calc days difference between two dates.
  *
  * @author: Nikolai Kostromitin
  */
 
 #include <iostream>
 
-
-/* Объявляю функции. Сами ф-ии находятся под ф-ей main() */
 bool is_valid_date(int day, int month, int year);
 int get_days_diff(int days1, int days2, int year1, int year2);
 bool is_leap(int year);
@@ -14,26 +12,24 @@ bool is_leap(int year);
 enum { FEB = 2, AUG = 8, SEP = 9, NOV = 11 };
 
 
-int main() // <- точка входа в программу
+int main()
 {
     const int avg_days_in_month = 30; // количество дней в месяце, в среднем
     const int ndates = 2; // число дат
 
     int total_days[ndates] = {}; // число дней с начала года до текущей даты
-    // {} - списковая инициализация - забивает массив дефолтными значениями;
-    // в данном случае - нулями.
     int years[ndates] = {};
 
-    // В цикле просто вычисляю число дней с начала года до каждой из дат, включительно.
-    for (int i = 0; i < ndates; ++i) { // повторяем для каждой даты
-        int day, month, year; // эти переменные видны только внутри цикла
-        std::cout << "Введите дату:    ";
+    // Вычисление числа дней с начала года до каждой из дат, включительно.
+    for (int i = 0; i < ndates; ++i) {
+        int day, month, year;
+        std::cout << "Input date:    ";
         std::cin >> day >> month >> year;
 
         years[i] = year;
 
         if (!is_valid_date(day, month, year))
-            return 1; // просто взял и вышел, если дата некорректна
+            return 1;
 
         total_days[i] = day + (month - 1) * avg_days_in_month;
 
@@ -45,28 +41,24 @@ int main() // <- точка входа в программу
 
         total_days[i] += month / 2; // добавляем месяцы с 31 днями
 
-        // для сентября и ноября наш способ подсчета числа месяцев с 31 днями
-        // не работает и нужно добавить +1 месяц (во всем виноват август)
+        /* для сентября и ноября наш способ подсчета числа месяцев с 31 днями
+           не работает и нужно добавить +1 месяц
+        */
         if (month == SEP || month == NOV)
             ++total_days[i];
 
-//#define DEBUG // отладочная инфа
 #ifdef DEBUG
-        // чтобы увидеть эти сообщения, раскомментируйте #define DEBUG
         std::cout << "DEBUG:\ttotal_days = " << total_days[i] << std::endl;
 #endif
     }
 
     int days_diff = get_days_diff(total_days[0], total_days[1], years[0], years[1]);
 
-    std::cout << "\nРазница в днях: " << days_diff << std::endl;
-} // <- точка выхода из программы
+    std::cout << "\nDays difference: " << days_diff << std::endl;
+}
 
 
-/* Определения функций находятся здесь */
-
-
-/* Проверка даты на корректность
+/* Check date for correctness.
  */
 bool is_valid_date(int d, int m, int y)
 {
@@ -84,10 +76,9 @@ bool is_valid_date(int d, int m, int y)
             return false;
     }
 
-    // Если мы здесь - значит дата корректна (по крайней мере, я на это надеюсь)
-    // (корректность февральских дат проверяется выше)
     return true;
 }
+
 
 /* Вычисляет разницу дней между двумя датами.
  * Каждая дата представлена в виде количества дней, прошедших с начала года,
@@ -106,7 +97,7 @@ int get_days_diff(int days1, int days2, int year1, int year2)
     return days_diff;
 }
 
-/* Определяет, является ли год високосным.
+/* Check if year is leap.
  */
 bool is_leap(int year)
 {
